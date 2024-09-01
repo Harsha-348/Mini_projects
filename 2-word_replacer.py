47e070cfef2c719f7696c51e799e69161af13e2e
Harsha-348 <harsha.echo348@gmail.com>
2024-09-01 13:46:00 +0530

    Add files via upload

---
+++ 2-word_replacer.py
@@ -0,0 +1,20 @@
+def word_replacer() :
+
+    sen="I did go to school today"
+    word_to_replace=str(input("enter the word to replace:"))
+    replacing_word=str(input("enter the word:"))
+    a=print(sen.replace(word_to_replace,replacing_word))
+    
+    
+word_replacer()
+
+jj="where are u?"
+print('''
+''')
+word_to_replace=str(input("enter the word to replace:"))
+replacing_word=str(input("enter the word:"))
+b=print(jj.replace(word_to_replace,replacing_word))
+
+    
+
+      
